5b5664d1bb155d1ff23fe67d704c4a5590b47ef8
夏天 <175629846+SummerXaa-Z@users.noreply.github.com>
2026-08-12 21:17:54 +0800

    feat: add access handoff projection

--- internal/httpapi/server_test.go
+++ internal/httpapi/server_test.go
@@ -861,6 +861,7 @@ func TestSystemInfoIncludesConsoleCompatibilityContract(t *testing.T) {
 		"permission_package_application_health",
 		"permission_package_application_impact",
 		"permission_package_production_readiness",
+		"permission_package_access_handoff_v1",
 		"permission_package_consumed_approval_recovery",
 		"management_mcp_tools_metadata_v4",
 	} {
